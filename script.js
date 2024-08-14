c4b5cbfa85a1365819dd1db523fc156225303e7c
OlgaMakeikina <makeikina2202@gmail.com>
2024-08-13 21:39:27 -0300

    добавленs отзывы

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const back = document.querySelector('#back');
 const next = document.querySelector('#next');
-const photos = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
+const photos = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"];
 let i=0;
 next.addEventListener('click', () => {
     i++;
